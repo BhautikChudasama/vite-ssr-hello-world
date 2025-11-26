f7012d06f1dd762fff1859b60ee519f10201a7e6
naman-dave <namandave@duck.com>
2025-11-26 14:00:41 +0530

    feat: done

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+
+export function App() {
+  return (
+    <div className="app">
+      <h1>Hello World</h1>
+      <p>This is a Vite + React SSR application!</p>
+    </div>
+  )
+}
